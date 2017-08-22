d14afcde02fd32eb5b62e774d13bf33ffa91776a
vladimir.kravchuk <v.kravchuk@aol.com>
2017-08-22 13:03:55 +0300

    small fix

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -46,7 +46,7 @@ module.exports = {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract(
                   'style',
-                  'css!sass',
+                  'css!sass'
                 ),
       },
       {
