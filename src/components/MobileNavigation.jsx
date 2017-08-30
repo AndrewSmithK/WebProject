2afdde3e23a506462e827bce32db1233026768a7
oleh.demedyuk <oleh.demeduyk@inveritasoft.com>
2017-08-30 13:04:41 +0300

    update mobile navigation

--- src/components/MobileNavigation.jsx
+++ src/components/MobileNavigation.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import IconClose from '../images/Header/icon-close.svg';
 import Logo from '../images/Header/logo-qover.svg';
@@ -10,6 +11,20 @@ export default class MobileNavigation extends Component {
       <div className="mob-nav">
         <div className="close-button" onClick={this.props.toglMobNav}><img src={IconClose} /></div>
         <div className="mobile-logo"><img src={Logo} /></div>
+        <div>
+          <ul className="nav-list">
+            <li><Link to="/">Homepage</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/about">Developers</Link></li>
+            <li><Link to="/contact-us">Contact us</Link></li>
+          </ul>
+        </div>
+        <div className="link-block">
+          <ul className="link-list">
+            <li><a href="#">Log in</a></li>
+            <li><a href="#">Request an access</a></li>
+          </ul>
+        </div>
       </div>
     );
   }
