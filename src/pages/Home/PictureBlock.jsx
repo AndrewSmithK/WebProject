6553a4806e4ae3ac3cedeb57e941baa037bcee32
vladimir.kravchuk <v.kravchuk@aol.com>
2017-09-01 13:51:28 +0300

    fix & clean

--- src/pages/Home/PictureBlock.jsx
+++ src/pages/Home/PictureBlock.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import ValueChain from '../../images/HomePage/value-chain-v-4.svg';
+
+import logo from '../../images/HomePage/logo-qover-turquoise.svg';
 
 export default class PictureBlock extends React.Component {
   render() {
-    return <section className="bg-gray text-center" id="picture-block">
-        <img src={ValueChain} alt="value-chain" />
+    return <section className="section bg-gray text-center" id="picture-block">
+      <h2 className="title">
+        <b>Qover's value chain:</b><br />more efficient & cheaper
+      </h2>
+      <div className="container">
+        <div className="infographic" />
+        <div className="descripts">
+          <div className="item text-left">
+            <h4>The Lloyd’s</h4>
+            <b>Loss Ratio</b><br />
+            <p>They focus on
+            carrying the risk</p>
+          </div>
+          <div className="item text-center">
+            <img src={logo} className="logo" alt="logo" />
+            <b>Expense Ratio</b><br />
+            <p>We focus on offering fully digital and much
+            more efficient products, pricing, IT and OPS</p>
+          </div>
+          <div className="item text-right">
+            <h4>You</h4>
+            <b>Commission Ratio</b><br />
+            <p>You focus on the relation-
+            ship with the customer</p>
+          </div>
+        </div>
+      </div>
     </section>;
   }
 }
