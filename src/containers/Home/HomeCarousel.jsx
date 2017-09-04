d5b51754c56ed1158ad387b919e0a66bb8810669
Vasyl <vasylherch@gmail.com>
2017-09-04 14:27:24 +0300

    add images to the slider

--- src/containers/Home/HomeCarousel.jsx
+++ src/containers/Home/HomeCarousel.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Slider from 'react-slick';
 
 import './HomeCarousel.scss';
-import image from '../../images/Header/logo-qover.svg';
+import emerce from '../../images/HomePage/Carousel/emerce.png';
+import echo from '../../images/HomePage/Carousel/echo.png';
+import techeu from '../../images/HomePage/Carousel/tech-eu.png';
+import detijd from '../../images/HomePage/Carousel/de-tijd.png';
+import datanews from '../../images/HomePage/Carousel/datanews.png';
 
 const NextArrow = (props) => {
   const { className, style, onClick } = props;
@@ -28,8 +32,9 @@ const PrevArrow = (props) => {
 
 export default () => {
   const settings = {
-    customPaging() {
-      return <a><img src={image} alt="test" /></a>;
+    customPaging(i) {
+      const images = [emerce, echo, techeu, detijd, datanews];
+      return <a><img src={images[i++]} alt="test" /></a>;
     },
     dots: true,
     infinite: true,
@@ -38,19 +43,30 @@ export default () => {
     slidesToScroll: 1,
     initialSlide: 2,
     nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
+    prevArrow: <PrevArrow />
   };
   return (
     <section id="home-carousel">
-      <div className="container">
-        <Slider {...settings}>
-          <div><h3>1</h3></div>
-          <div><h3>2</h3></div>
-          <div><h3>3</h3></div>
-          <div><h3>4</h3></div>
-          <div><h3>5</h3></div>
-        </Slider>
-      </div>
+      <Slider {...settings}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div>
+          <h1>
+            The whole world is talking about Qover
+          </h1>
+          <div>
+            « Belgian financial technology startup Qover has raised €5.5 million in a Series A round ledby Anthemis. »
+          </div>
+          <div>
+            Read the full article
+          </div>
+          <div>
+            Jonathan Keane, Tech. EU
+          </div>
+        </div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+      </Slider>
     </section>
   );
 };
