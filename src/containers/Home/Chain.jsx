bd0fb34cb2a057f592a36f70a2e121a5bf23618a
vladimir.kravchuk <v.kravchuk@aol.com>
2017-09-07 14:37:40 +0300

    phraseapp changes

---
+++ src/containers/Home/Chain.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl-phraseapp';
+
+import logo from '../../images/HomePage/logo-qover-turquoise.svg';
+
+export default class Chain extends React.Component {
+  render() {
+    return <section className="section bg-gray text-center" id="picture-block">
+      <h2 className="title">
+        <b>
+          <FormattedMessage 
+            id={`home.chain.title.part1`}
+            defaultMessage={`Qover's value chain:`}
+          />
+        </b><br />
+        <FormattedMessage 
+          id={`home.chain.title.part1`}
+          defaultMessage={`more efficient & cheaper`}
+        />
+      </h2>
+      <div className="container">
+        <div className="infographic" />
+        <div className="descripts">
+          <div className="item text-left">
+            <h4>
+              <FormattedMessage 
+                id={`home.chain.theLoyds`}
+                defaultMessage={`The Lloyd’s`}
+              />
+            </h4>
+            <b>
+              <FormattedMessage 
+                id={`home.chain.lossRatio`}
+                defaultMessage={`Loss Ratio`}
+              />
+            </b><br />
+            <p>
+              <FormattedMessage 
+                id={`home.chain.theyFocus`}
+                defaultMessage={`They focus on
+                carrying the risk`}
+              />
+            </p>
+          </div>
+          <div className="item text-center">
+            <img src={logo} className="logo" alt="logo" />
+            <b>
+              <FormattedMessage 
+                id={`home.chain.expenseRatio`}
+                defaultMessage={`Expense Ratio`}
+              />
+            </b><br />
+            <p>
+              <FormattedMessage 
+                id={`home.chain.weFocus`}
+                defaultMessage={`We focus on offering fully digital and much
+                more efficient products, pricing, IT and OPS`}
+              />
+            </p>
+          </div>
+          <div className="item text-right">
+            <h4>
+              <FormattedMessage 
+                id={`home.chain.you`}
+                defaultMessage={`You`}
+              />
+            </h4>
+            <b>
+              <FormattedMessage 
+                id={`home.chain.commissionRatio`}
+                defaultMessage={`Commission Ratio`}
+              />
+            </b><br />
+            <p>
+              <FormattedMessage 
+                id={`home.chain.youFocus`}
+                defaultMessage={`You focus on the relation-
+                ship with the customer`}
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>;
+  }
+}
--- src/containers/Home/RobustApi.jsx
+++ src/containers/Home/RobustApi.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl-phraseapp';
 
 import Api from '../../images/HomePage/RobustApi/api-image.jpg';
 import IconDevBig from '../../images/HomePage/RobustApi/icon-dev-big.svg';
@@ -12,8 +13,14 @@ export default class RobustApi extends React.Component {
         <img src={IconDevBig} alt="Qover - icon-dev-big"/>
       </div>
       <h2 className="title text-grey">
-        Easy to use and robust API that makes <br/>
-        insurance distribution effortless
+        <FormattedMessage 
+          id={`home.robustApi.title.part1`}
+          defaultMessage={`Easy to use and robust API that makes`}
+        /><br/>
+        <FormattedMessage 
+          id={`home.robustApi.title.part1`}
+          defaultMessage={`insurance distribution effortless`}
+        />
       </h2>
       <div className="gap-image">
         <img src={Api} className="img-fluid" alt="Qover - api" />
@@ -23,23 +30,38 @@ export default class RobustApi extends React.Component {
           <div className="col-md-5">
             <div className="item">
               <h5 className="title text-grey">
-                Developers first
+                <FormattedMessage 
+                  id={`home.robustApi.item1.title`}
+                  defaultMessage={`Developers first`}
+                />
               </h5>
               <p className="text text-grey">
-                We continously seek out elegant solutions that enable robust, scalable and flexible integrations. We manage the complexity on our side so that you can get up and running with Qover in few minutes.
+                <FormattedMessage 
+                  id={`home.robustApi.item1.text`}
+                  defaultMessage={`We continously seek out elegant solutions that enable robust, scalable and flexible integrations. We manage the complexity on our side so that you can get up and running with Qover in few minutes.`}
+                />
               </p>
               <Link className="link" to="#">
-                See the API documentation<i className="fa fa-angle-right" aria-hidden="true"></i>
+                <FormattedMessage 
+                  id={`home.robustApi.link`}
+                  defaultMessage={`See the API documentation`}
+                /><i className="fa fa-angle-right" aria-hidden="true"></i>
               </Link>
             </div>
           </div>
           <div className="col-md-5">
             <div className="item">
               <h5 className="title text-grey">
-                Always improving
+                <FormattedMessage 
+                  id={`home.robustApi.item2.title`}
+                  defaultMessage={`Always improving`}
+                />
               </h5>
               <p className="text text-grey">
-                Our world-class ninjas are constantly improving any fact of the Qover Stack. We develop new features, new products and new countries every months. We love it: #Hacking-Insurance.
+                <FormattedMessage 
+                  id={`home.robustApi.item2.text`}
+                  defaultMessage={`Our world-class ninjas are constantly improving any fact of the Qover Stack. We develop new features, new products and new countries every months. We love it: #Hacking-Insurance.`}
+                />
               </p>
             </div>
           </div>
