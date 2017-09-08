dbdb7fb3add643be72fd05708ee673954b2cc87a
vladimir.kravchuk <v.kravchuk@aol.com>
2017-09-08 15:45:44 +0300

    paralax start

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {IntlProvider} from 'react-intl';
 import {initializePhraseAppEditor} from 'react-intl-phraseapp';
+import { ParallaxController } from 'react-scroll-parallax';
 
 import $ from 'jquery'
 import Popper from 'popper.js'
@@ -15,6 +16,7 @@ import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 import initializePhraseAppEnabled from './scripts/phraceappEnable'
 
+ParallaxController.init();
 injectTapEventPlugin()
 initializePhraseAppEnabled()
 
@@ -35,13 +37,13 @@ const store = configureStore(initialState)
 
 ReactDOM.render(
   <Provider store={store}>
-    <IntlProvider locale="en">
       <MuiThemeProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <IntlProvider locale="en">
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </IntlProvider>
       </MuiThemeProvider>
-    </IntlProvider>
   </Provider>
 , document.getElementById('root')
 )
