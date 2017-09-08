9d0d6ad4eec77d3a171f4c5075f9f8b283aee3f5
Vasyl <vasylherch@gmail.com>
2017-09-08 11:11:31 +0300

    fix links on term

--- src/containers/TermsPolicies/components/TermsItem.jsx
+++ src/containers/TermsPolicies/components/TermsItem.jsx
@@ -1,33 +1,13 @@
+import React from 'react';
+import { Link, Route } from 'react-router-dom'
 
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
-export default class TermsItem extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      active: false,
-    };
-    this.activeState = this.activeState.bind(this);
-  }
-
-  activeState(){
-    this.setState({
-      active: !this.state.active,
-    });
-  }
-
-  render() {
-    return (
-      <div className={`${this.state.active ? 'active' : ''} item-title`} onClick={this.activeState}>
-        <Link to={this.props.path}>
-          {this.props.title}
-        </Link>
-        <ul>
-          {this.props.children}
-        </ul>
-      </div>
-    );
-  }
-}
-
+export default ({ title, path, exact, children }) => (
+  <Route path={path} exact={exact} children={({ match }) => (
+    <div className={`item-title ${match ? 'active' : ''}`}>
+      <Link to={path}>{title}</Link>
+      <ul>
+        {children}
+      </ul>
+    </div>
+  )} />
+)
