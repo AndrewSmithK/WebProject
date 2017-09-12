fec1cb4982979efbdd76e68cc3288d7b42c66757
Mykhailo Shevchenko <mikleshef@gmail.com>
2017-09-12 13:47:05 +0300

    Zoom for pictures

--- src/containers/OurProducts/ProductItem.js
+++ src/containers/OurProducts/ProductItem.js
@@ -3,38 +3,38 @@ import { FormattedMessage } from 'react-intl-phraseapp'
 
 export default (props) => (
     <div className="col-sm-4 col-6">
-        <div className="item text-center bg-white">
-            <div className="icon">
-                <img src={props.image} alt="accident-health" />
-            </div>
-            <div className="text-container">
-                <h5 className="title">
-                    <FormattedMessage
-                        id={`products.traditional.${props.num}.title`}
-                        defaultMessage={props.title}
-                    />
-                </h5>
-                <p className="text">
-                    <FormattedMessage
-                        id={`products.traditional.${props.num}.text`}
-                        defaultMessage={props.text}
-                    />
-                </p>
-            </div>
-            {props.status === 'soon' ?
-                <button className="btn btn-disabled">
-                    <FormattedMessage
-                        id={`products.traditional.ComingSoon`}
-                        defaultMessage={`Coming soon`}
-                    />
-                </button> :
-                <button className="btn">
-                    <FormattedMessage
-                        id={`products.traditional.ComingSoon`}
-                        defaultMessage={`More info`}
-                    />
-                </button>
-            }
+      <div className="item text-center bg-white">
+        <div className="icon">
+          <img src={props.image} alt="accident-health" />
         </div>
+        <div className="text-container">
+          <h5 className="title">
+            <FormattedMessage
+              id={`products.traditional.${props.num}.title`}
+              defaultMessage={props.title}
+            />
+          </h5>
+          <p className="text">
+            <FormattedMessage
+              id={`products.traditional.${props.num}.text`}
+              defaultMessage={props.text}
+            />
+          </p>
+        </div>
+        {props.status === 'soon' ?
+          <button className="btn btn-disabled">
+            <FormattedMessage
+              id={`products.traditional.ComingSoon`}
+              defaultMessage={`Coming soon`}
+            />
+          </button> :
+          <button className="btn">
+            <FormattedMessage
+              id={`products.traditional.ComingSoon`}
+              defaultMessage={`More info`}
+            />
+          </button>
+        }
+      </div>
     </div>
 )
