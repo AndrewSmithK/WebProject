e9842b64c7a9449b83fda794766f7259e831748c
Mykhailo Shevchenko <mikleshef@gmail.com>
2017-09-12 21:10:43 +0300

    RegEx fixed

--- src/containers/Registration/index.jsx
+++ src/containers/Registration/index.jsx
@@ -448,14 +448,14 @@ export default class Registration extends Component {
 
                           <div className="form__field form__field">
                             <TextValidator style={{left:'12px'}}
-                                        {...textFieldSettings}
-                                        value={formData.number}
-                                        onChange={this.handleChange}
-                                        floatingLabelStyle={{ display: 'none' }}
-                                        floatingLabelText="Number"
-                                        name="number"
-                                        validators={['required', 'matchRegexp:^[0-9]$']}
-                                        errorMessages={['this field is required', 'The format of the phone number is not correct. Please respect the format « 499 12 34 56 ».']}
+                              {...textFieldSettings}
+                              value={formData.number}
+                              onChange={this.handleChange}
+                              floatingLabelStyle={{ display: 'none' }}
+                              floatingLabelText="Number"
+                              name="number"
+                              validators={['required', 'matchRegexp:[0-9]$']}
+                              errorMessages={['this field is required', 'The format of the phone number is not correct. Please respect the format « 499 12 34 56 ».']}
                                     />
                                 </div>
                             </div>
