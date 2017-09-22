de835ff0f114b6abf5fe29102ec20c48800f2d17
Vasyl <vasylherch@gmail.com>
2017-09-22 15:19:49 +0300

    QOV-245

--- src/containers/Home/GapInsurance.jsx
+++ src/containers/Home/GapInsurance.jsx
@@ -22,15 +22,15 @@ export default class GapInsurance extends React.Component {
             <li className="nav-item">
               <a className="nav-link active" href="#insurance" role="tab" data-toggle="tab">Gap Insurance</a>
             </li>
-            {/* <li className="nav-item">
-              <a className="nav-link" href="#light-casco" role="tab" data-toggle="tab">Light Casco</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#light-casco" role="tab" data-toggle="tab">Accident & Health</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#full-casco" role="tab" data-toggle="tab">Full Casco</a>
+              <a className="nav-link" href="#full-casco" role="tab" data-toggle="tab">Travel</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#driver-accident" role="tab" data-toggle="tab">Driver Accident</a>
-            </li> */}
+              <a className="nav-link" href="#driver-accident" role="tab" data-toggle="tab">Job Loss</a>
+            </li>
           </ul>
         </div>
       </div>
@@ -85,7 +85,7 @@ export default class GapInsurance extends React.Component {
                     />
                   </p>
                 </DropdownItem>
-                <DropdownItem  style={{ marginBottom: '20px' }} title={{
+                <DropdownItem style={{ marginBottom: '20px' }} title={{
                   id: "home.gap.part3.title",
                   text: "We sublime the customer experience"
                 }}>
@@ -127,26 +127,347 @@ export default class GapInsurance extends React.Component {
                     />
                   </Link>
                   <Link className="link text-white" to="/registration">
-                <FormattedMessage
-                  id={`home.gap.link`}
-                  defaultMessage={`Become a partner`}
-                />
-                <i className="fa fa-angle-right" aria-hidden="true" />
-              </Link>
+                    <FormattedMessage
+                      id={`home.gap.link`}
+                      defaultMessage={`Become a partner`}
+                    />
+                    <i className="fa fa-angle-right" aria-hidden="true" />
+                  </Link>
+                </div>
+              </div>
+              <div className="col-12 col-md-6">
+                <div className="big-image">
+                  <img src={Mockup} alt="mockup" />
+                </div>
+              </div>
             </div>
           </div>
-          <div className="col-12 col-md-6">
-            <div className="big-image">
-              <img src={Mockup} alt="mockup" />
-            </div>
-          </div>
-        </div>
-      </div>
 
         </div>
-        <div role="tabpanel" className="tab-pane" id="light-casco">2</div>
-        <div role="tabpanel" className="tab-pane" id="full-casco">3</div>
-        <div role="tabpanel" className="tab-pane" id="driver-accident">4</div>
+        <div role="tabpanel" className="tab-pane" id="light-casco">
+        <div className="container text-white">
+            <div className="row">
+              <div className="left-block col-12 col-md-6">
+                <h3 className="subtitle">
+                  <small className="text-turquoise">
+                    <FormattedMessage
+                      id={`home.gap.subtitle`}
+                      defaultMessage={`Discover our first product:`}
+                    />
+                  </small><br />
+                  <FormattedMessage
+                    id={`home.gap.title`}
+                    defaultMessage={`Be one of the first to introduce the
+                Motor GAP Insurance in Belgium.`}
+                  />
+                </h3>
+                <DropdownItem title={{
+                  id: "home.gap.part1.title",
+                  text: "What is GAP Insurance?"
+                }}>
+                  <p className="text">
+                    <FormattedMessage
+                      id={`home.gap.part1.p1`}
+                      defaultMessage={`Get back 100% of the purchase price of a car up to 5-years.`}
+                    />
+                    <br /><br />
+                    <FormattedMessage
+                      id={`home.gap.part1.p2`}
+                      defaultMessage={`The GAP insurance covers the potential financial shotfall between the
+                  purchase price of the car and the indemnity paid by the casco insurer
+                  in case the vehicle is written off.`}
+                    />
+                  </p>
+                </DropdownItem>
+                <DropdownItem title={{
+                  id: "home.gap.part2.title",
+                  text: "Target customer:"
+                }}>
+                  <p className="text what-desc">
+                    <FormattedMessage
+                      id={`home.gap.part2.listitem1`}
+                      defaultMessage={`For new and used cars bought in the last 6 months`}
+                    /><br />
+                    <FormattedMessage
+                      id={`home.gap.part2.listitem2`}
+                      defaultMessage={`Must have a casco insurance`}
+                    />
+                  </p>
+                </DropdownItem>
+                <DropdownItem style={{ marginBottom: '20px' }} title={{
+                  id: "home.gap.part3.title",
+                  text: "We sublime the customer experience"
+                }}>
+                  <div className="icon-label-list">
+                    <div className="icon-label">
+                      <img src={IconCoverage} alt="icon-coverage" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem1`}
+                          defaultMessage={`Up to 60 months`}
+                        />
+                      </span>
+                    </div>
+                    <div className="icon-label" style={{ float: 'right' }}>
+                      <img src={IconCancel} alt="icon-cancel" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem2`}
+                          defaultMessage={`Cancel anytime`}
+                        />
+                      </span>
+                    </div>
+                    <div className="icon-label">
+                      <img src={IconReferralSmall} alt="icon-referral_small" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem3`}
+                          defaultMessage={`Digital, efficient and much (much) cheaper`}
+                        />
+                      </span>
+                    </div>
+                  </div>
+                </DropdownItem>
+                <div>
+                  <Link className="btn button btn-turquoise" to="/our-products/gap-insurance">
+                    <FormattedMessage
+                      id={`home.gap.btnMore`}
+                      defaultMessage={`More info about the GAP`}
+                    />
+                  </Link>
+                  <Link className="link text-white" to="/registration">
+                    <FormattedMessage
+                      id={`home.gap.link`}
+                      defaultMessage={`Become a partner`}
+                    />
+                    <i className="fa fa-angle-right" aria-hidden="true" />
+                  </Link>
+                </div>
+              </div>
+              <div className="col-12 col-md-6">
+                <div className="big-image">
+                  <img src={Mockup} alt="mockup" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div role="tabpanel" className="tab-pane" id="full-casco">
+        <div className="container text-white">
+            <div className="row">
+              <div className="left-block col-12 col-md-6">
+                <h3 className="subtitle">
+                  <small className="text-turquoise">
+                    <FormattedMessage
+                      id={`home.gap.subtitle`}
+                      defaultMessage={`Discover our first product:`}
+                    />
+                  </small><br />
+                  <FormattedMessage
+                    id={`home.gap.title`}
+                    defaultMessage={`Be one of the first to introduce the
+                Motor GAP Insurance in Belgium.`}
+                  />
+                </h3>
+                <DropdownItem title={{
+                  id: "home.gap.part1.title",
+                  text: "What is GAP Insurance?"
+                }}>
+                  <p className="text">
+                    <FormattedMessage
+                      id={`home.gap.part1.p1`}
+                      defaultMessage={`Get back 100% of the purchase price of a car up to 5-years.`}
+                    />
+                    <br /><br />
+                    <FormattedMessage
+                      id={`home.gap.part1.p2`}
+                      defaultMessage={`The GAP insurance covers the potential financial shotfall between the
+                  purchase price of the car and the indemnity paid by the casco insurer
+                  in case the vehicle is written off.`}
+                    />
+                  </p>
+                </DropdownItem>
+                <DropdownItem title={{
+                  id: "home.gap.part2.title",
+                  text: "Target customer:"
+                }}>
+                  <p className="text what-desc">
+                    <FormattedMessage
+                      id={`home.gap.part2.listitem1`}
+                      defaultMessage={`For new and used cars bought in the last 6 months`}
+                    /><br />
+                    <FormattedMessage
+                      id={`home.gap.part2.listitem2`}
+                      defaultMessage={`Must have a casco insurance`}
+                    />
+                  </p>
+                </DropdownItem>
+                <DropdownItem style={{ marginBottom: '20px' }} title={{
+                  id: "home.gap.part3.title",
+                  text: "We sublime the customer experience"
+                }}>
+                  <div className="icon-label-list">
+                    <div className="icon-label">
+                      <img src={IconCoverage} alt="icon-coverage" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem1`}
+                          defaultMessage={`Up to 60 months`}
+                        />
+                      </span>
+                    </div>
+                    <div className="icon-label" style={{ float: 'right' }}>
+                      <img src={IconCancel} alt="icon-cancel" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem2`}
+                          defaultMessage={`Cancel anytime`}
+                        />
+                      </span>
+                    </div>
+                    <div className="icon-label">
+                      <img src={IconReferralSmall} alt="icon-referral_small" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem3`}
+                          defaultMessage={`Digital, efficient and much (much) cheaper`}
+                        />
+                      </span>
+                    </div>
+                  </div>
+                </DropdownItem>
+                <div>
+                  <Link className="btn button btn-turquoise" to="/our-products/gap-insurance">
+                    <FormattedMessage
+                      id={`home.gap.btnMore`}
+                      defaultMessage={`More info about the GAP`}
+                    />
+                  </Link>
+                  <Link className="link text-white" to="/registration">
+                    <FormattedMessage
+                      id={`home.gap.link`}
+                      defaultMessage={`Become a partner`}
+                    />
+                    <i className="fa fa-angle-right" aria-hidden="true" />
+                  </Link>
+                </div>
+              </div>
+              <div className="col-12 col-md-6">
+                <div className="big-image">
+                  <img src={Mockup} alt="mockup" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div role="tabpanel" className="tab-pane" id="driver-accident">
+        <div className="container text-white">
+            <div className="row">
+              <div className="left-block col-12 col-md-6">
+                <h3 className="subtitle">
+                  <small className="text-turquoise">
+                    <FormattedMessage
+                      id={`home.gap.subtitle`}
+                      defaultMessage={`Discover our first product:`}
+                    />
+                  </small><br />
+                  <FormattedMessage
+                    id={`home.gap.title`}
+                    defaultMessage={`Be one of the first to introduce the
+                Motor GAP Insurance in Belgium.`}
+                  />
+                </h3>
+                <DropdownItem title={{
+                  id: "home.gap.part1.title",
+                  text: "What is GAP Insurance?"
+                }}>
+                  <p className="text">
+                    <FormattedMessage
+                      id={`home.gap.part1.p1`}
+                      defaultMessage={`Get back 100% of the purchase price of a car up to 5-years.`}
+                    />
+                    <br /><br />
+                    <FormattedMessage
+                      id={`home.gap.part1.p2`}
+                      defaultMessage={`The GAP insurance covers the potential financial shotfall between the
+                  purchase price of the car and the indemnity paid by the casco insurer
+                  in case the vehicle is written off.`}
+                    />
+                  </p>
+                </DropdownItem>
+                <DropdownItem title={{
+                  id: "home.gap.part2.title",
+                  text: "Target customer:"
+                }}>
+                  <p className="text what-desc">
+                    <FormattedMessage
+                      id={`home.gap.part2.listitem1`}
+                      defaultMessage={`For new and used cars bought in the last 6 months`}
+                    /><br />
+                    <FormattedMessage
+                      id={`home.gap.part2.listitem2`}
+                      defaultMessage={`Must have a casco insurance`}
+                    />
+                  </p>
+                </DropdownItem>
+                <DropdownItem style={{ marginBottom: '20px' }} title={{
+                  id: "home.gap.part3.title",
+                  text: "We sublime the customer experience"
+                }}>
+                  <div className="icon-label-list">
+                    <div className="icon-label">
+                      <img src={IconCoverage} alt="icon-coverage" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem1`}
+                          defaultMessage={`Up to 60 months`}
+                        />
+                      </span>
+                    </div>
+                    <div className="icon-label" style={{ float: 'right' }}>
+                      <img src={IconCancel} alt="icon-cancel" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem2`}
+                          defaultMessage={`Cancel anytime`}
+                        />
+                      </span>
+                    </div>
+                    <div className="icon-label">
+                      <img src={IconReferralSmall} alt="icon-referral_small" />
+                      <span>
+                        <FormattedMessage
+                          id={`home.gap.part3.listitem3`}
+                          defaultMessage={`Digital, efficient and much (much) cheaper`}
+                        />
+                      </span>
+                    </div>
+                  </div>
+                </DropdownItem>
+                <div>
+                  <Link className="btn button btn-turquoise" to="/our-products/gap-insurance">
+                    <FormattedMessage
+                      id={`home.gap.btnMore`}
+                      defaultMessage={`More info about the GAP`}
+                    />
+                  </Link>
+                  <Link className="link text-white" to="/registration">
+                    <FormattedMessage
+                      id={`home.gap.link`}
+                      defaultMessage={`Become a partner`}
+                    />
+                    <i className="fa fa-angle-right" aria-hidden="true" />
+                  </Link>
+                </div>
+              </div>
+              <div className="col-12 col-md-6">
+                <div className="big-image">
+                  <img src={Mockup} alt="mockup" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
 
